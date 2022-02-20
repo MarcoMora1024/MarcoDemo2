bacb09d18704b3e99bb6997691157e6baaac8c99
MarcoMora1024 <marcoalonso1024@gmail.com>
2022-02-20 11:01:26 -0600

    Se agregó el encabezado y el pie de pagina

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+    <footer class="footer">
+      <div class="container content has-text-centered">
+        <p>Hecho con el corazón 1</p>
+
+      </div>
+    </footer>
+  `,
+  styles: [
+  ]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
